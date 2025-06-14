521b926cf76ceab28e8a716da744936fc487bb6a
Tyler Weeres <tweeres04@gmail.com>
2025-06-13 17:50:10 -0700

    Pick a sans-serif font

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
